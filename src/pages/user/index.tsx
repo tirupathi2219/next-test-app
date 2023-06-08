bf74085b8cb988175db8dd4248be7942c232bde8
tirupathi2219 <tirupathi2219@gmail.com>
2023-06-08 08:35:59 +0530

    key-add

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -5,8 +5,8 @@ export default function UserInfo() {
     const data = ['user1', 'user2', 'user3', 'user4']
     return (
         <>
-          {data.map((item) => {
-            return <h3><Link href={`/user/${item}`}>{item}</Link></h3>
+          {data.map((item,index) => {
+            return <h3 key={index}><Link href={`/user/${item}`}>{item}</Link></h3>
           })}
           <div><Goback/></div>
         </>
